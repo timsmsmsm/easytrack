326135df42bf7a89ccbdc8a9fa269d962fdb32f0
Pablo Vicente Munuera <pablovm1990@gmail.com>
2026-01-20 13:15:30 +0100

    Adding tests for GUI widgets

--- tests/test_optim_widget.py
+++ tests/test_optim_widget.py
@@ -1,5 +1,7 @@
 """Tests for optim widget module."""
+from __future__ import annotations
 
+from collections.abc import Callable
 import numpy as np
 import pytest
 from pathlib import Path
@@ -10,9 +12,17 @@ from src.napari_easytrack.widgets.optim_widget import (
     BtrackOptimizationWidget,
 )
 
-@pytest.fixture
-def optim_widget():
-    """Fixture for BtrackOptimizationWidget."""
-    widget = BtrackOptimizationWidget()
-    yield widget
-    widget.close()
+
+def test_add_optimization_widget(
+    make_napari_viewer: Callable,
+):
+    """Checks that the optimization widget can be added inside a dock widget."""
+
+    viewer = make_napari_viewer()
+    num_dw = len(viewer.window._dock_widgets)
+    viewer.window.add_plugin_dock_widget(
+        plugin_name="napari-easytrack",
+        widget_name="Parameter Tuning",
+    )
+
+    assert len(viewer.window._dock_widgets) == num_dw + 1
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,4 +1,5 @@
 """Tests for widget module."""
+from __future__ import annotations
 
 import numpy as np
 import pytest
@@ -6,8 +7,30 @@ from pathlib import Path
 import tempfile
 import os
 
+from collections.abc import Callable
+from unittest.mock import patch
+
+import numpy as np
+from numpy.testing import assert_allclose
+
+import napari
+
 from src.napari_easytrack.widgets.widget import (
     BtrackPresetWidget,
 )
+
+def test_add_segmentation_widget(
+    make_napari_viewer: Callable,
+):
+    """Checks that the segmentation widget can be added inside a dock widget."""
+
+    viewer = make_napari_viewer()
+    num_dw = len(viewer.window._dock_widgets)
+    viewer.window.add_plugin_dock_widget(
+        plugin_name="napari-easytrack",
+        widget_name="Tracking",
+    )
+
+    assert len(viewer.window._dock_widgets) == num_dw + 1
 
 
